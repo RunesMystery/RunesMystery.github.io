ce14576a9ea96a0686f976fb86f877fbc9865749
CuteZilla <82705888+CuteZilla@users.noreply.github.com>
2021-09-10 20:10:53 -0700

    react update

--- home.js
+++
@@ -1,7 +0,0 @@
-import abi from "./abi.js";
-
-const web3 = new Web3('http://localhost:8545');
-let RuneContract = new web3.eth.Contract(abi, "0x2F8281050849075077aB0b2fbAc6630654f6dA0e");
-
-console.log(RuneContract);
-console.log(RuneContract.totalSupply())
